c2f09cfa4a7ef161dae183092ebd0290b127a441
rimivan <rimola7@gmail.com>
2020-05-12 16:02:40 +0200

    changed sw

--- static/sw.js
+++ static/sw.js
@@ -1,28 +1,17 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+const workbox = new WorkboxSW();
 
 if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-  workbox.routing.registerRoute('https://fonts.googleapis.com/(.*)',
-    workbox.strategies.cacheFirst({
-      cacheName: 'google-font-cache',
-      cacheExpiration: {
-        maxEntries: 30
-      },
-      cacheableResponse: {statuses: [0, 200]}
-    })
-  );
-  
-  workbox.routing.registerRoute(/\.(?:png|gif|jpg|svg)$/,
-    workbox.strategies.cacheFirst({
-      cacheName: 'images-cache'
-    })
-  );
-  
-  workbox.routing.registerRoute(/\.(?:js|css)$/,
-    workbox.strategies.staleWhileRevalidate({
-      cacheName: 'static-resources-cache'
-    })
-  );
+    console.log(`Yay! Workbox is loaded 🎉`);
+    workbox.router.registerRoute('https://fonts.googleapis.com/(.*)',
+        workbox.strategies.cacheFirst({
+            cacheName: 'googleapis',
+            cacheExpiration: {
+                maxEntries: 30
+            },
+            cacheableResponse: {statuses: [0, 200]}
+        })
+    );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
