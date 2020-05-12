1b6641163a84ef0496b58a5558382db282884bad
rimivan <rimola7@gmail.com>
2020-05-12 16:15:44 +0200

    changed sw

--- static/sw.js
+++ static/sw.js
@@ -2,13 +2,25 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
-        
     // Cache js/css files.
     workbox.routing.registerRoute(
         /\.(?:js|css)$/,
-        new workbox.strategies.StaleWhileRevalidate(),
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'static-rsrc-cache'
+        }),
     ); 
-  
+    workbox.routing.registerRoute(
+        /\.(?:png|jpeg|jpg)$/,
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: 'imgs-cache'
+        }),
+    ); 
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.googleapis\.com/,
+        new workbox.strategies.StaleWhileRevalidate({
+          cacheName: 'google-fonts-cache',
+        }),
+    );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
