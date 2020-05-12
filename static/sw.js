4d192b6ce6a9f971af4ae10cd0e803aa5e8c74c6
rimivan <rimola7@gmail.com>
2020-05-12 16:09:01 +0200

    sw changes

--- static/sw.js
+++ static/sw.js
@@ -1,17 +1,14 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-const workbox = new WorkboxSW();
 
 if (workbox) {
     console.log(`Yay! Workbox is loaded 🎉`);
-    workbox.router.registerRoute('https://fonts.googleapis.com/(.*)',
-        workbox.strategies.cacheFirst({
-            cacheName: 'googleapis',
-            cacheExpiration: {
-                maxEntries: 30
-            },
-            cacheableResponse: {statuses: [0, 200]}
-        })
-    );
+        
+    // Cache js/css files.
+    workbox.routing.registerRoute(
+        /\.(?:js|css)$/,
+        new workbox.strategies.StaleWhileRevalidate(),
+    ); 
+  
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
