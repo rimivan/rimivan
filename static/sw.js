8b153fc27206b3f50187852f2517da56cb1f52a2
rimivan <rimola7@gmail.com>
2020-05-12 15:57:41 +0200

    routing name

--- static/sw.js
+++ static/sw.js
@@ -2,28 +2,27 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
+  workbox.routing.registerRoute('https://fonts.googleapis.com/(.*)',
+    workbox.strategies.cacheFirst({
+      cacheName: 'google-font-cache',
+      cacheExpiration: {
+        maxEntries: 30
+      },
+      cacheableResponse: {statuses: [0, 200]}
+    })
+  );
+  
+  workbox.routing.registerRoute(/\.(?:png|gif|jpg|svg)$/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images-cache'
+    })
+  );
+  
+  workbox.routing.registerRoute(/\.(?:js|css)$/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'static-resources-cache'
+    })
+  );
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
-
-workbox.router.registerRoute('https://fonts.googleapis.com/(.*)',
-  workbox.strategies.cacheFirst({
-    cacheName: 'google-font-cache',
-    cacheExpiration: {
-      maxEntries: 30
-    },
-    cacheableResponse: {statuses: [0, 200]}
-  })
-);
-
-workbox.router.registerRoute(/\.(?:png|gif|jpg|svg)$/,
-  workbox.strategies.cacheFirst({
-    cacheName: 'images-cache'
-  })
-);
-
-workbox.router.registerRoute(/\.(?:js|css)$/,
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: 'static-resources-cache'
-  })
-);
